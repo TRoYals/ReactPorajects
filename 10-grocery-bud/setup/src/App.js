3047db28d9fdf7b814a88c8e46be1f4340d6a8cb
Naglfar28 <aozora00321@gmail.com>
2023-01-09 09:44:29 +0800

    todo list project

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,98 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [task, setTask] = useState("");
+  const [items, setItems] = useState([]);
+  const [editId, setEditId] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+  const handleDelete = (id) => {
+    const allTasks = [...items];
+    allTasks.splice(id, 1);
+    setItems(allTasks);
+    showAlert(true, "danger", "delete done");
+  };
+  const handleEdit = (item, id) => {
+    setTask(item);
+
+    setIsEdit(true);
+    setEditId(id);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!task) {
+      showAlert(true, "danger", "please enter value");
+      return;
+    }
+    const allTasks = [...items];
+    if (isEdit) {
+      allTasks[editId] = task;
+      setItems(allTasks);
+      setEditId("");
+      setIsEdit(false);
+      setTask("");
+      showAlert(true, "success", "update done");
+      return;
+    }
+    setItems([...items, task]);
+    showAlert(true, "success", "create done");
+    setTask("");
+  };
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert}></Alert>}
+        <h3>Todo List</h3>
+
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="Task1"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button className="submit-btn">submit</button>
+        </div>
+      </form>
+      {items.length > 0 && (
+        <div className="grocery-container">
+          <div className="grocery-list">
+            {items.map((item, index) => {
+              return (
+                <List
+                  item={item}
+                  id={index}
+                  key={index}
+                  handleEdit={handleEdit}
+                  handleDelete={handleDelete}
+                ></List>
+              );
+            })}
+          </div>
+          <button
+            className="clear-btn"
+            onClick={() => {
+              setItems("");
+              showAlert(true, "danger", "delete all");
+            }}
+          >
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default App
+export default App;
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,27 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ item, id, handleEdit, handleDelete }) => {
+  return (
+    <article className="grocery-item">
+      <p className="title">{item}</p>
+      <div className="btn-container">
+        <button
+          className="edit-btn"
+          type="button"
+          onClick={() => handleEdit(item, id)}
+        >
+          <FaEdit></FaEdit>
+        </button>
+        <button
+          className="delete-btn"
+          type="button"
+          onClick={() => handleDelete(id)}
+        >
+          <FaTrash></FaTrash>
+        </button>
+      </div>
+    </article>
+  );
+};
 
-export default List
+export default List;
