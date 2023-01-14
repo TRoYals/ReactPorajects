f321285552b5a63ecd3750f0517278eb11eefc74
Naglfar28 <aozora00321@gmail.com>
2023-01-14 11:00:12 +0800

    sidebar submenu; hover motion, useCotent

--- 13-stripe-submenus/setup/src/Sidebar.js
+++ 13-stripe-submenus/setup/src/Sidebar.js
@@ -1,9 +1,43 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import sublinks from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+  const { closeSidebar, isSidebarOpen } = useGlobalContext();
 
-export default Sidebar
+  return (
+    <div
+      className={`${
+        isSidebarOpen ? "sidebar-wrapper show" : "sidebar-wrapper "
+      }`}
+    >
+      <di className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes></FaTimes>
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((link, index) => {
+            return (
+              <article key={index}>
+                <h4>{link.page}</h4>
+                <div className="sidebar-sublinks">
+                  {link.links.map((item, index) => {
+                    return (
+                      <a href={item.url} key={index}>
+                        {item.icon}
+                        {item.label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </di>
+    </div>
+  );
+};
+
+export default Sidebar;
