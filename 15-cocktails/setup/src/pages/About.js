d8b7ec794b851f450071e5fc0af6a0becf38bc67
Naglfar28 <aozora00321@gmail.com>
2023-01-28 08:05:00 +0800

    cocktail project

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, sit
+        voluptatum id distinctio non in atque aperiam repellat nobis? Recusandae
+        maiores laboriosam labore quam, enim repudiandae ducimus commodi
+        veritatis odio?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
