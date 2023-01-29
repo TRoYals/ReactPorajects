1e0170c7f4fe14314cec49dc57e81e462a2fe59f
Naglfar28 <aozora00321@gmail.com>
2023-01-29 11:27:55 +0800

    pagination project

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -1,8 +1,73 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState();
+
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+    console.log(followers);
+  }, [page, loading]);
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+
+  const selectPage = (index) => {
+    setPage(index);
+  };
+  return (
+    <main>
+      <div className="section-title">
+        <h1>pagination</h1>
+      </div>
+      <div className="followers">
+        <div className="container">
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
+        </div>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPage}>
+            Prev
+          </button>
+          {data.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className={`page-btn ${page === index ? "active - btn" : ""}`}
+                onClick={() => selectPage(index)}
+              >
+                {index}
+              </button>
+            );
+          })}
+          <button className="next-btn" onClick={nextPage}>
+            Next
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,12 @@
-const paginate = () => {}
+// paginate the json file
+const paginate = (data) => {
+  const followerPerPage = 10;
+  const pages = Math.ceil(data.length / followerPerPage);
+  const dataPerPage = Array.from({ length: pages }, (_, index) => {
+    const start = index * followerPerPage;
+    return data.slice(start, start + followerPerPage);
+  });
+  return dataPerPage;
+};
 
-export default paginate
+export default paginate;
