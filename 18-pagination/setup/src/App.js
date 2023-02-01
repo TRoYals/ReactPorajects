b1081ab34b4072ab581f32b4236ab1f4b9962d15
Naglfar28 <aozora00321@gmail.com>
2023-02-01 11:32:20 +0800

    pagination renew

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -4,13 +4,12 @@ import Follower from "./Follower";
 function App() {
   const { loading, data } = useFetch();
   const [page, setPage] = useState(0);
-  const [followers, setFollowers] = useState();
+  const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
     if (loading) return;
     setFollowers(data[page]);
-    console.log(followers);
-  }, [page, loading]);
+  }, [loading, page]);
 
   const nextPage = () => {
     setPage((oldPage) => {
@@ -40,32 +39,34 @@ function App() {
       <div className="section-title">
         <h1>pagination</h1>
       </div>
-      <div className="followers">
+      <section className="followers">
         <div className="container">
           {followers.map((follower) => {
             return <Follower key={follower.id} {...follower} />;
           })}
         </div>
-        <div className="btn-container">
-          <button className="prev-btn" onClick={prevPage}>
-            Prev
-          </button>
-          {data.map((item, index) => {
-            return (
-              <button
-                key={index}
-                className={`page-btn ${page === index ? "active - btn" : ""}`}
-                onClick={() => selectPage(index)}
-              >
-                {index}
-              </button>
-            );
-          })}
-          <button className="next-btn" onClick={nextPage}>
-            Next
-          </button>
-        </div>
-      </div>
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              Prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${page === index ? "active - btn" : ""}`}
+                  onClick={() => selectPage(index)}
+                >
+                  {index}
+                </button>
+              );
+            })}
+            <button className="next-btn" onClick={nextPage}>
+              Next
+            </button>
+          </div>
+        )}
+      </section>
     </main>
   );
 }
