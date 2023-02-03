67e5d58fab47babe27b50eda42858d215531d18b
Naglfar28 <aozora00321@gmail.com>
2023-02-03 21:22:07 +0800

    hacker news API, useReduce

--- 22-hacker-news/setup/src/context.js
+++ 22-hacker-news/setup/src/context.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useReducer } from 'react'
+import React, { useContext, useEffect, useReducer } from "react";
 
 import {
   SET_LOADING,
@@ -6,21 +6,65 @@ import {
   REMOVE_STORY,
   HANDLE_PAGE,
   HANDLE_SEARCH,
-} from './actions'
-import reducer from './reducer'
+} from "./actions";
+import reducer from "./reducer";
 
-const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
 
-const initialState = {}
+const initialState = {
+  isLoading: true,
+  query: "python",
+  page: 0,
+};
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchStories = async (url) => {
+    dispatch({ type: SET_LOADING });
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      dispatch({
+        type: SET_STORIES,
+        payload: {
+          data: data.hits,
+          nbPages: data.nbPages,
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleSearch = async (query) => {
+    dispatch({ type: HANDLE_SEARCH, payload: query });
+  };
+
+  const removeStory = (objectID) => {
+    dispatch({ type: REMOVE_STORY, payload: objectID });
+  };
+  const handlePage = (pageChange) => {
+    dispatch({ type: HANDLE_PAGE, payload: pageChange });
+  };
+
+  useEffect(() => {
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
+  }, [state.query, state.page]);
+
+  return (
+    <AppContext.Provider
+      value={{ ...state, handleSearch, removeStory, handlePage }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
